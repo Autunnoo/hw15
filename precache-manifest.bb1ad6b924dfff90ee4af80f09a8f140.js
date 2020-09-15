b7648ce2c15175ce25e2d60ce2b1a3b14d06b93a
Autunnoo <68438045+Autunnoo@users.noreply.github.com>
2020-09-15 15:17:24 +0300

    Updates

---
+++ precache-manifest.bb1ad6b924dfff90ee4af80f09a8f140.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092b7a2b65e55d1af7fb89f4b3ab0a76",
+    "url": "/hw15/index.html"
+  },
+  {
+    "revision": "823ef3092ed15911cb64",
+    "url": "/hw15/static/css/main.dafff242.chunk.css"
+  },
+  {
+    "revision": "f990eed95582b90a50ec",
+    "url": "/hw15/static/js/2.932072ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw15/static/js/2.932072ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "823ef3092ed15911cb64",
+    "url": "/hw15/static/js/main.212134ad.chunk.js"
+  },
+  {
+    "revision": "25714a02588382265cdb",
+    "url": "/hw15/static/js/runtime-main.130117e2.js"
+  },
+  {
+    "revision": "6254420da120a8adab8583898dcbcd9a",
+    "url": "/hw15/static/media/galochka.6254420d.png"
+  },
+  {
+    "revision": "a37c5d0be56da5d6b3339af384306af0",
+    "url": "/hw15/static/media/like.a37c5d0b.png"
+  },
+  {
+    "revision": "a4881eb9aead2b6f08b22776f238f171",
+    "url": "/hw15/static/media/message.a4881eb9.png"
+  },
+  {
+    "revision": "9761d2ae0838ad7dce8bc607d050ef1b",
+    "url": "/hw15/static/media/repost.9761d2ae.png"
+  }
+]);
